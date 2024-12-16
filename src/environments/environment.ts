08e37cf41e5c357abe9790d0d2fd20888b5057b6
Hanzala Sarguroh <hanzalasarguroh@gmail.com>
2024-12-17 03:36:38 +0530

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@ export const environment ={
     projectId: 'password-manager-73d4d',
     appId: '1:884055226579:web:d178d5de4295f8a392fc26',
     storageBucket: 'password-manager-73d4d.appspot.com',
-    apiKey: 'AIzaSyD-P8BxMMp5mxKPe-YqEEQ9dc7Is2AVV9E',
+    apiKey: '',
     authDomain: 'password-manager-73d4d.firebaseapp.com',
     messagingSenderId: '884055226579',
   },
